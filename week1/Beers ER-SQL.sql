fdfd5d796d2558616685f556b910d28dbbbe0c0a
YawenRiva <41601162+YawenRiva@users.noreply.github.com>
2019-02-21 14:41:33 +1100

    Create Beers ER-SQL.sql

---
+++ week1/Beers ER-SQL.sql
@@ -0,0 +1,43 @@
+/*convert the design to something thats implementable*/
+-- Beer Database SQL Schema
+
+-- ENTITIES
+create table Beers (
+    name    text, -- or varchar(100)
+    manf    text not null, -- or varchar(80)
+    primary key (name)
+);
+create table Bars (
+    name    text,
+    addr    text not null,
+    -- licence# has a particular format and always 10 character long
+    -- so use fixed-length 'char(n)'
+    lic_no  char(10) not null,
+    primary key (name)
+);
+create table Drinkers (
+    name    varchar(60), -- or text
+    phone   integer not null,
+    addr    char(15) not null,
+    primary key(name)
+);
+
+-- RELATIONSHIPS
+create table sells (
+    price   float not null,
+    beers   text references Beers(name),
+    bars    text references Bars(name),
+    primary key(beers, bars)
+);
+create table likes (
+    -- if the type of attribute is VARCHAR/CHAR()
+    -- the length need to be the same as the length in above entity table
+    beers   text references Beers(name),
+    drinkers    text references Drinkers(name),
+    primary key(beers, drinkers)
+);
+create table frequents (
+    bars   text references Bars(name),
+    drinkers    text references Drinkers(name),
+    primary key(bars, drinkers)
+);
