b4d0239b1cd825d8c1336af522b900f6b1a53491
YawenRiva <41601162+YawenRiva@users.noreply.github.com>
2019-02-22 10:25:05 +1100

    update comments about varchar/text and primary key

--- week1/Beers ER-SQL.sql
+++ week1/Beers ER-SQL.sql
@@ -1,6 +1,10 @@
 /*convert the design to something thats implementable*/
 -- Beer Database SQL Schema
 
+
+/*TEXT has fixed max size (same as CHAR)
+  VARCHAR() has variable max size
+  if have MAX size known, varchar(n) would be a better choice*/
 -- ENTITIES
 create table Beers (
     name    text, -- or varchar(100)
@@ -23,12 +27,17 @@ create table Drinkers (
 );
 
 -- RELATIONSHIPS
+/*
+  PRIMARY KEY is for ENTITY only
+  NOT FOR RELATIONSHIP SETS
+*/
 create table sells (
     price   float not null,
     beers   text references Beers(name),
     bars    text references Bars(name),
     primary key(beers, bars)
 );
+
 create table likes (
     -- if the type of attribute is VARCHAR/CHAR()
     -- the length need to be the same as the length in above entity table
@@ -36,6 +45,7 @@ create table likes (
     drinkers    text references Drinkers(name),
     primary key(beers, drinkers)
 );
+
 create table frequents (
     bars   text references Bars(name),
     drinkers    text references Drinkers(name),
