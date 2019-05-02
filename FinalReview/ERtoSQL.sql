7547e2f62747ae7d418df2a4aecf1331385c7251
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-05-02 16:16:47 +1000

    Update ERtoSQL.sql

--- FinalReview/ERtoSQL.sql
+++ FinalReview/ERtoSQL.sql
@@ -25,6 +25,38 @@
  CREATE TABLE HOBBIES (
       StuID     integer,
       hobbies   text,
-      foreign key (StuID) references to STUDENT(StuID),
+      foreign key (StuID) references STUDENT(StuID),
       Primary key (StuID, hobbies)
  );
+
+
+--EXAMPLE 3
+--STUDENT ENROLS IN CLASS MANY TO MANY
+CREATE TABLE ENROLS (
+     student    integer,
+     course     integer,
+     foreign key (student) references Students(id),
+     foreign key (courses) references Course(id),
+     primary key (student, course)
+);
+
+--EXAMPLE 4
+--COURSE HAS ONE CONVENOR WHO IS A STAFF MEMBER one(tp)->one
+CREATE TABLE COURSE (
+     id    integer primary key,
+     ...
+     convenor integer not null references STAFF(id),
+);
+
+--EXAMPLE 5
+-- ONE STAFF MANAGES ONE BRANCH, BRANCH HAS TO HAVE A MANAGER
+CREATE TABLE BRANCH (
+     id    integer primary key,
+     ...
+     manager  integer not nullreferences STAFF(id)
+);
+CREATE TABLE STAFF (
+    id    integer primary key,
+    ...
+    manages    integer not null references BRANCH(id)
+);
