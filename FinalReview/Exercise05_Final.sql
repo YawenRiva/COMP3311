d132377cef48eadf0f63ba132a74abbf965950ac
YAWEN LUO <41601162+YawenRiva@users.noreply.github.com>
2019-05-06 15:44:57 +1000

    Create Exercise05_Final.sql

---
+++ FinalReview/Exercise05_Final.sql
@@ -0,0 +1,7 @@
+/*
+Person(id:integer, ..., name:text, ...)
+Student(id:integer, sid:integer, stype:('local','intl'))
+Staff(id:integer, sid:integer, office:integer, ...)
+Term(id:integer, year:integer, session:('S1','S2','X1','X2'), ...)
+Subject(id:integer, code:text, ..., name:text, ... uoc:integer, ...)
+Course(id:integer, subject:integer, term:integer, lic:integer, ...)*/
